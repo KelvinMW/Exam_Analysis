101272fd1b72c389f64ac41dad0fbc3f3d083b92
Kelvin Maina <mainakelvin360@gmail.com>
2023-02-28 21:14:10 +0300

    Analysis Module:multiselect on courses and classes/subject

--- analysis_view.php
+++ analysis_view.php
@@ -26,54 +26,44 @@ if (isActionAccessible($guid, $connection2, '/modules/Exam Analysis/analysis_vie
     $page->addError(__('You do not have access to this action.'));
 }
 else{
-        // School Year Info
-        $gibbonSchoolYearID = $_GET['gibbonSchoolYearID'] ?? $session->get('gibbonSchoolYearID');
-        $gibbonCourseID = $_GET['gibbonCourseID'] ?? null;
-  /*
-    $form = Form::create('examAnalysis', $session->get('absoluteURL').'/index.php','get');
-    $form->setFactory(DatabaseFormFactory::create($pdo));
-    $form->addHiddenValue('address', $session->get('address'));
-//select analysis form
-$row = $form->addRow();
-$row->addLabel('examType', __('Select Exam Type'))
-    ->description(__('Select the type of exam'));
-$types = $settingGateway->getSettingByScope('Formal Assessment', 'internalAssessmentTypes');
-$row->addSelect('type')->fromString($types)->required()->placeholder();
-// Get the form inputs from the user
-
-*/
+  // School Year Info
+ $gibbonSchoolYearID = $_GET['gibbonSchoolYearID'] ?? $session->get('gibbonSchoolYearID');
+ $gibbonCourseID = $_GET['gibbonCourseID'] ?? null;
+ 
 echo 'Kindly select the information of the exam you want to analyse :';
-//<?php
-//$form = Form::create('action', $_SESSION[$guid]['absoluteURL'].'/modules/'.$_SESSION[$guid]['module'].'/thing.php');
-                
-//$form->addHiddenValue('address', $_SESSION[$guid]['address']);
-
-//$row = $form->addRow();
-//    $row->addFooter();
- //   $row->addSubmit();
-
-//echo $form->getOutput();
-
+//create form object
 $form = Form::create('examAnalysis', $session->get('absoluteURL').'/analysis_view.php','get');
 $form->setFactory(DatabaseFormFactory::create($pdo));
 $form->addHiddenValue('address', $session->get('address'));
 //select analysis form
+
+//type of exam
+$types = $settingGateway->getSettingByScope('Formal Assessment', 'internalAssessmentTypes');
     $row = $form->addRow();
     $row->addLabel('examType', __('Select Exam Type'))
-        ->description(__('Select the type of exam'));
-$types = $settingGateway->getSettingByScope('Formal Assessment', 'internalAssessmentTypes');
+        ->description(__('Select the type of exam'))
+        ->required();
     $row->addSelect('type')->fromString($types)->required()->placeholder();
 
-    $row = $form->addRow();
+//the Form group or class
+    $row = $form->addRow()->addClass('course bg-blue-100');
     $row->addLabel('gibbonFormGroupID', __('Form Group'));
-    $row->addSelectFormGroup('gibbonFormGroupID', $session->get('gibbonSchoolYearID'))->required()->selected($gibbonFormGroupID)->placeholder();
+    $row->addSelectFormGroup('gibbonFormGroupID', $session->get('gibbonSchoolYearID'))->required()->selectMultiple()->selected($gibbonFormGroupID)->placeholder();
+
 //add courses
 $data = array('gibbonSchoolYearID' => $session->get('gibbonSchoolYearID'));
 $sql = "SELECT gibbonCourseID as value, nameShort as name FROM gibbonCourse WHERE gibbonSchoolYearID=:gibbonSchoolYearID ORDER BY name";
 $row = $form->addRow()->addClass('course bg-blue-100');
 $row->addLabel('courses[]', __('Select Courses'));
-$row->addSelect('courses[]')->fromQuery($pdo, $sql, $data)->selectMultiple()->setSize(6)->required()->selected($selected);
-
+$row->addSelect('courses[]')->fromQuery($pdo, $sql, $data)
+    ->selectMultiple()
+    ->setSize(6)
+    ->required()
+    ->selected($selected);
+//go
+$row = $form->addRow();
+$row->addSearchSubmit($gibbon->session, __('Clear Filters'), array('gibbonSchoolYearID'));
+$form->loadAllValuesFrom($request);
 echo $form->getOutput();
 
 
