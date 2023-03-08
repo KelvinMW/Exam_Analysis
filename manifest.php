4ce3f691c6de5198ecb134e903131069e222260d
Kelvin Maina <mainakelvin360@gmail.com>
2023-03-08 22:50:08 +0300

    Exam Analysis Moduel: fixed manifest

--- manifest.php
+++ manifest.php
@@ -22,7 +22,7 @@ along with this program.  If not, see <http:// www.gnu.org/licenses/>.
 // Basic variables
 $name        = 'Exam Analysis';            // The name of the module as it appears to users. Needs to be unique to installation. Also the name of the folder that holds the unit.
 $description = 'This module is for administrators to do exam analysis';            // Short text description
-$entryURL    = "anasysis_view.php";   // The landing page for the unit, used in the main menu
+$entryURL    = "analysis_view.php";   // The landing page for the unit, used in the main menu
 $type        = "Additional";  // Do not change.
 $category    = 'Assess';            // The main menu area to place the module in
 $version     = '1.0.00';            // Version number
@@ -59,6 +59,7 @@ $actionRows[] = [
 ];
 // Action rows 
 // One array per action
+/*
 $actionRows[] = [
     'name'                      => 'Graphical Analysis', // The name of the action (appears to user in the right hand side module menu)
     'precedence'                => '0',// If it is a grouped action, the precedence controls which is highest action in group
@@ -80,3 +81,4 @@ $actionRows[] = [
 ];
 // Hooks 
 $hooks[] = ''; // Serialised array to create hook and set options. See Hooks documentation online.
+*/
