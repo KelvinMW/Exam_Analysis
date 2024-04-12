dbda0bed79c4871f80aa6ef5b07f7229a1a84990
kelvin <wkelvinmaina@gmail.com>
2024-04-12 14:42:41 +0300

    Mean Deviations:include export csv

--- moduleFunctions.php
+++ moduleFunctions.php
@@ -91,6 +91,23 @@ function calculateDeviations($meanScores, $examType1, $examType2) {
         $stmt->execute(['gibbonCourseID' => $gibbonCourseID]);
         return $stmt->fetchColumn();
     }
+    function arrayToCsvDownload($array, $filename = "export.csv", $delimiter=";") {
+        // Open raw memory as file so no temp files needed, you might run out of memory though
+        $f = fopen('php://memory', 'w'); 
+        // loop over the input array
+        foreach ($array as $line) { 
+            // generate csv lines from the inner arrays
+            fputcsv($f, $line, $delimiter); 
+        }
+        // reset the file pointer to the start of the file
+        fseek($f, 0);
+        // tell the browser it's going to be a csv file
+        header('Content-Type: application/csv');
+        // tell the browser we want to save it instead of displaying it
+        header('Content-Disposition: attachment; filename="'.$filename.'";');
+        // make php send the generated csv lines to the browser
+        fpassthru($f);
+    }    
 // modify this function to dynamically generate the courses name from the selected courses  
 function renderHtmlTable($meanScores, $deviations, $examType1, $examType2) {
     // Begin table HTML
@@ -145,8 +162,57 @@ function renderHtmlTable($meanScores, $deviations, $examType1, $examType2) {
         $html .= "<td>$meanDeviation</td></tr>";
     }
 
-    $html .= "</table>";
-    return $html;
+// Add a button with an onclick event handler to call your export function
+$html .= '<button onclick="exportTableToCSV(\'export.csv\')">Export to Excel</button>';
+    
+$html .= '</table>';
+// Add JavaScript function for export
+$html .= '<script>
+    function exportTableToCSV(filename) {
+        var csv = [];
+        var rows = document.querySelectorAll("table tr");
+        
+        for (var i = 0; i < rows.length; i++) {
+            var row = [], cols = rows[i].querySelectorAll("td, th");
+            
+            for (var j = 0; j < cols.length; j++) 
+                row.push(cols[j].innerText);
+            
+            csv.push(row.join(","));
+        }
+
+        // Download CSV file
+        downloadCSV(csv.join("\n"), filename);
+    }
+    
+    function downloadCSV(csv, filename) {
+        var csvFile;
+        var downloadLink;
+    
+        // CSV file
+        csvFile = new Blob([csv], {type: "text/csv"});
+    
+        // Download link
+        downloadLink = document.createElement("a");
+    
+        // File name
+        downloadLink.download = filename;
+    
+        // Create a link to the file
+        downloadLink.href = window.URL.createObjectURL(csvFile);
+    
+        // Hide download link
+        downloadLink.style.display = "none";
+    
+        // Add the link to DOM
+        document.body.appendChild(downloadLink);
+    
+        // Click download link
+        downloadLink.click();
+    }
+</script>';
+
+return $html;
 }
      
     // Helper function to get form group name by ID
