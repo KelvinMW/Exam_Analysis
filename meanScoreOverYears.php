6628554eab7d8ece9c5f7e8d9b7bc5416b7b18ba
Kelvin Maina <wkelvinmaina@gmail.com>
2024-08-30 14:04:25 +0300

    Update meanScoreOverYears.php

--- meanScoreOverYears.php
+++ meanScoreOverYears.php
@@ -80,7 +80,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Exam Analysis/meanScoreOve
   seamless
   frameBorder="0"
   scrolling="no"
-  src="https://data.elmischools.com/superset/explore/p/6WeEWZalrwb/?standalone=1&height=400"
+  src="https://data.elmischools.com/superset/explore/p/m7WDj0JLEzx/?standalone=1&height=400"
 >
 </iframe>
 	    </tr>
